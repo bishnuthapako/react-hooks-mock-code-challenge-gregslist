635075b20269ea25d1e0e18c54ce325aecc56360
Bishnu <bishnuthapako@gmail.com>
2022-07-04 19:22:23 -0400

    bug resolve

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [loadData, setLoadData]=useState([])
+
+
+
+ useEffect(()=>{
+  fetch("http://localhost:6001/listings")
+  .then((res)=>res.json())
+  .then((data)=>{
+    // console.log(data)
+    setLoadData(data)
+
+  })
+
+
+ },[])
+
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer loadData={loadData}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({loadData}) {
+
+
   return (
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
+      
+      {loadData.map((data)=><ListingCard key={data.id} description={data.description} image={data.image} location={data.location}/>)}
       </ul>
     </main>
   );
